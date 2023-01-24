48b8f6554eece51f9c5b71c139fab7d24c7abb51
cdgmx <iamcdgmx@gmail.com>
2023-01-24 16:14:11 +0800

    Add ios insider

---
+++ ios/InsiderNotificationContent/NotificationViewController.h
@@ -0,0 +1,12 @@
+//
+//  NotificationViewController.h
+//  InsiderNotificationContent
+//
+//  Created by christianmo on 1/24/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationViewController : UIViewController
+
+@end
---
+++ ios/InsiderNotificationService/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  InsiderNotificationService
+//
+//  Created by christianmo on 1/24/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
